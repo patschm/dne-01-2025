55a1586aa5c5231bdea7e1ce03ec3b3954bd0984
unknown <pschmidt@pasit.nl>
2025-01-14 13:15:36 +0100

    Day2

--- Exercises/Exercise 4/Starter/Calculator/CalculatorApp.cs
+++ Exercises/Exercise 4/Starter/Calculator/CalculatorApp.cs
@@ -2,17 +2,26 @@ namespace Calculator;
 
 public partial class CalculatorApp : Form
 {
+    private readonly SynchronizationContext? _sync;
+
     public CalculatorApp()
     {
         InitializeComponent();
+        _sync = SynchronizationContext.Current;
     }
 
-    private async void button1_Click(object sender, EventArgs e)
+    private void button1_Click(object sender, EventArgs e)
     {
+        //var jacco = SynchronizationContext.Current
         if (int.TryParse(txtA.Text, out int a) && int.TryParse(txtB.Text, out int b)) 
         {
-            var result = LongAdd(a, b);
-            UpdateAnswer(result);
+            //var result = LongAdd(a, b);
+            //UpdateAnswer(result);
+            Task.Run(()=>LongAdd(a, b))
+                .ContinueWith(t => {
+                    _sync.Send(UpdateAnswer, t.Result);
+                }
+                );
         }
     }
 
@@ -25,9 +34,5 @@ public partial class CalculatorApp : Form
     {
         Task.Delay(10000).Wait();
         return a + b;
-    }
-    private Task<int> LongAddAsync(int a, int b)
-    {       
-        return Task.Run(()=>LongAdd(a,b));
     }
 }
--- Live/Module_4/Draden/Program.cs
+++ Live/Module_4/Draden/Program.cs
@@ -1,9 +1,4 @@
 ﻿
-
-
-
-using System.Threading.Channels;
-
 namespace Draden;
 
 internal class Program
@@ -13,11 +8,82 @@ internal class Program
         //OrigineelProbleem();
         //TaskDemo();
         //TaskMetResultaat();
-        TaskChaining();
+        //TaskChaining();
+        //TaskFouten();
+        //TaskCancels();
+        TaskKewl();
 
         Console.WriteLine("Einde programma");
         //Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
         Console.ReadLine();
+    }
+
+    private static async void TaskKewl()
+    {
+        Task<int> t1 = new Task<int>(() => LongAdd(4, 5));
+        t1.Start();
+
+        Console.WriteLine("Voor 1");
+        //var x = t1.Result;
+        var x = await t1;
+        Console.WriteLine($"Na 1 {x}");
+
+        var x2 = await LongAddAsync(6, 9);
+        Console.WriteLine(x2);
+
+        try
+        {
+            await Task.Run(() => {
+                Console.WriteLine("Start");
+                throw new Exception("Ooops");
+            });
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+
+    private static void TaskCancels()
+    {
+        CancellationTokenSource nikko = new CancellationTokenSource();
+
+        CancellationToken bommetje = nikko.Token;
+        Task.Run(() =>
+        {
+            for (int i = 0; ; i++)
+            {
+                Console.WriteLine($"Doe iets {i}");
+                Task.Delay(500).Wait();
+                if (bommetje.IsCancellationRequested)
+                {
+                    return;
+                }
+            }
+        });
+
+        nikko.CancelAfter(5000);
+    }
+
+    private static void TaskFouten()
+    {
+       // try
+        //{
+            Task.Run(() => {
+                Console.WriteLine("Start");
+                throw new Exception("Ooops");
+            }).ContinueWith(pt=> {
+                Console.WriteLine(pt.Status);
+                if (pt.Exception != null)
+                {
+                    Console.WriteLine(pt.Exception.InnerException);
+                }
+            });
+       // }
+        //catch (Exception ex)
+        //{
+            //Console.WriteLine(ex.ToString());
+        //}
     }
 
     private static void TaskChaining()
@@ -39,7 +105,7 @@ internal class Program
         });
         t1.ContinueWith(pt => Console.WriteLine("Andere"));
 
-        LongAddSpecial(10, 20).ContinueWith(pt => Console.WriteLine(pt.Result));
+        LongAddAsync(10, 20).ContinueWith(pt => Console.WriteLine(pt.Result));
     }
 
     private static void TaskMetResultaat()
@@ -84,12 +150,11 @@ internal class Program
         return a + b;
     }
 
-    static Task<int> LongAddSpecial(int a, int b)
+    static Task<int> LongAddAsync(int a, int b)
     {
         Task<int> t1 = new Task<int>(() =>
         {
             int result = LongAdd(a, b);
-            //Console.WriteLine($"Het resultaat is: {result}");
             return result;
         });
 
